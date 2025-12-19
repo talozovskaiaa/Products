b3c7e1723eefaf7f4e09a14972b8b243805377ae
Татьяна Лозовская <tanya-lozovskaya@yandex.ru>
2025-12-19 23:51:02 +0700

    Adding Class

---
+++ Holiday_Project/src/main/java/ru/netology/Main.java
@@ -0,0 +1,13 @@
+package ru.netology;
+
+public class Main {
+    public String present;
+    public String family;
+    public String mood;
+
+    public Main(String present, String family, String mood) {
+        this.present = present;
+        this.family = family;
+        this.mood = mood;
+    }
+}
---
+++ Holiday_Project/src/main/java/ru/netology/SubMainClass.java
@@ -0,0 +1,10 @@
+package ru.netology;
+
+public class SubMainClass {
+    public static void main(String[] args) {
+        Main[] main = new Main[3];
+        main[0] = new Main("книга", "Васильевы", "праздничный");
+        main[1] = new Main("цветы", "Петровы", "уютный");
+        main[2] = new Main("игрушка", "Сидоровы", "родной");
+    }
+}
