136b478a984e171693b87b64a3c62f801189333c
Татьяна Лозовская <tanya-lozovskaya@yandex.ru>
2026-02-24 22:19:21 +0700

    add BookPriceComparator

--- Products/src/main/java/ru/netology/Book.java
+++ Products/src/main/java/ru/netology/Book.java
@@ -1,13 +1,19 @@
 package ru.netology;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Book extends Product {
     protected String author;
     protected String booksName;
+    protected int price;
 
     public Book(int id, String name, int price, String booksName, String author) {
         super(id, name, price);
         this.booksName = booksName;
         this.author = author;
+        this.price = price;
     }
 
     @Override
@@ -28,4 +34,32 @@ public class Book extends Product {
     public String getBooksName() {
         return booksName;
     }
+
+    /**
+     * Метод для получения цены
+     */
+    public int getPrice() {
+        return price;
+    }
+
+    public static void main(String[] args) {
+
+    Book book1 = new Book(1, "Java Basics", 25, "Java Basics 1.0", "author1");
+    Book book2 = new Book(2, "PHP Basics", 26, "PHP Basics", "author2");
+    Book book3 = new Book(3, "Javascript Basics", 27, "Javascript Basics", "author3");
+    Book book4 = new Book(3, "Python Basics", 28, "Python Basics", "author4");
+
+    List<Book> books = new ArrayList<>();
+        books.add(book1);
+        books.add(book2);
+        books.add(book3);
+        books.add(book4);
+
+        Collections.sort(books, new BookPriceComparator());
+
+        for (Book book : books) {
+            System.out.println(book);
+        }
+    }
 }
+
---
+++ Products/src/main/java/ru/netology/BookPriceComparator.java
@@ -0,0 +1,17 @@
+package ru.netology;
+
+import java.util.Comparator;
+
+public class BookPriceComparator implements Comparator<Book> {
+
+    @Override
+    public int compare(Book o1, Book o2) {
+        if (o1.getPrice() < o2.getPrice()) {
+            return -1;
+        } else if (o1.getPrice() > o2.getPrice()) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+}
